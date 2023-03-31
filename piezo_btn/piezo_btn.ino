d6b8790aa175fbdb7a8a09333f492eb0234a0ff4
Seola <wonseola123@gmail.com>
2023-03-31 11:53:29 +0900

    piezo_btn

--- piezo_btn/piezo_btn.ino
+++ piezo_btn/piezo_btn.ino
@@ -2,6 +2,7 @@ const int bpin=10;
 int btnStart=2;
 int states=0;
 int tones[] = {262,293,329,349,392,440,493,523};
+
 void play(){
   for(int i = 0 ; i<8; i++){
     tone(bpin,tones[i]);
@@ -19,8 +20,5 @@ void loop() {
 states=digitalRead(btnStart);
 if (states==1){
   play();
-}
-else{
-  noTone(bpin);
 }
 }
